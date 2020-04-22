9dda8003c6369b8730e58d060b1faff5a82fbc87
Claudio <sapliv2016@gmail.com>
2020-04-21 22:47:32 -0400

    Incluye fecha y nuevo formato de output

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ data = res.json()['data']
 df = pd.DataFrame(columns=['price'])
 
 for attribute in data:
-    df.loc[ attribute['attributes']['date'] ] = attribute['attributes']['price']
+    df.loc[attribute['attributes']['date'] ] = attribute['attributes']['price']
 
 df.index = pd.to_datetime(df.index, format="%Y/%m/%d")
 
@@ -23,17 +23,14 @@ p_eod = df['price'].loc[df.index[0] + BusinessDay(-1)]
 p_eom = df['price'].loc[df.index[0] + MonthEnd(-1)]
 p_eoy = df['price'].loc[df.index[0] + YearEnd(-1)]
 
-# Final Printout
-output = 'Risky Norris P&L {} DTD, {} MTD, {} YTD'.format(
-    round(100*(p/p_eod-1),2),
-    round(100*(p/p_eom-1),2),
-    round(100*(p/p_eoy-1),2),
-    )
-
 
 @app.route('/')
 def hello():
-    return output
+    print(df.index[0].date())
+    return "Risky Norris {} P&L <br/> \
+            {:10.2f}% DTD  <br/>  \
+            {:10.2f}% MTD  <br/> \
+            {:10.2f}% YTD  <br/>".format(df.index[0].date(), p/p_eod-1, p/p_eom, p/p_eoy)
 
 if __name__ == '__main__':
     app.run()
